56c3f3ce69e5bf5a8e056f34cede4e1318885c77
blurryface2809 <grazimashado@gmail.com>
2022-10-03 16:33:45 -0300

    exercicios

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+
+
+
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById("fruta-1")
+fruta1.innerHTML = "laranja"
+const fruta2 = document.getElementById("fruta-2")
+fruta2.innerHTML = "limão"
+const fruta3 = document.getElementById("fruta-3")
+fruta3.innerHTML = "uva"
+
+
+const funcaoFrutas = () =>{
+    const fruta1 = document.getElementById("fruta1")
+    const fruta2 = document.getElementById("fruta2")
+    const fruta3 = document.getElementById("fruta3")
+    console.log(`${fruta1.value}, ${fruta2.value}, ${fruta3.value}`)
+    fruta1.value = ""
+    fruta2.value = ""
+    fruta3.value = ""
+}
